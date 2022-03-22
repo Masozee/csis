11c10c1fe24ac1604793a395b3ae0d30c235df1b
masozee <nurojilukmansyah@gmail.com>
2022-03-22 22:54:45 +0700

    mega menu

--- web/context_processors.py
+++ web/context_processors.py
@@ -1,6 +1,19 @@
-from .models import Publication 
+from .models import Department, Publication, Publication_category
 
 
 def menu(request):
-    menu = Publication.objects.all()
-    return {'menu': menu}
+    recent_publication = Publication.objects.filter(publish=True).order_by('date_created')[:1]
+    menu_publication = Publication.objects.filter(publish=True).order_by('-date_created')[:8]
+    menu_dept = Department.objects.filter(publish=True)
+    menu_cat = Publication_category.objects.all()
+
+
+    context = {
+        "menu_pub": menu_publication,
+        "menu_dep":menu_dept,
+        "cat": menu_cat,
+        "recent_pub": recent_publication,
+
+    }
+    return context
+   
