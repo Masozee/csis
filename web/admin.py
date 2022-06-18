fb7661bf3dc63765def580df01fe6f11d3cded9d
masozee <nurojilukmansyah@gmail.com>
2022-06-18 11:44:33 +0700

    add admin and models for BOD

--- web/admin.py
+++ web/admin.py
@@ -1,16 +1,22 @@
 from unicodedata import category
 from django.contrib import admin
 from django.utils.html import format_html
-from jmespath import search
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 from .models import *
 
-class deptAdmin(admin.ModelAdmin):
+
+@admin.action(description='Mark selected stories as published')
+def make_published(modeladmin, request, queryset):
+    queryset.update(publish=True)
+
+
+class deptAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     search_fields = ['name']
 admin.site.register(Department, deptAdmin)
 
-class topicAdmin(admin.ModelAdmin):
+class topicAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ('name', 'parent', 'date_created', 'date_modified', 'publish')
     search_fields = ['name',]
     date_hierarchy = 'date_created'
@@ -18,7 +24,7 @@ class topicAdmin(admin.ModelAdmin):
     autocomplete_fields = ['parent']
 admin.site.register(Topic, topicAdmin)
 
-class projectadmin(admin.ModelAdmin):
+class projectadmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ('title', 'dept','member', 'periode_start', 'periode_end', 'publish')
     list_filter = ('publish','department')
     search_fields = ['title','department','project_member' ]
@@ -37,13 +43,14 @@ admin.site.register(Project, projectadmin)
 
 
 
-class eventadmin(admin.ModelAdmin):
+class eventadmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ('title', 'dept','date_start','date_end','speakers', 'project', 'publish')
     list_filter = ('publish','department')
     search_fields = ['title',]
     date_hierarchy = 'date_created'
     readonly_fields = ('date_created', 'date_modified')
     autocomplete_fields = ['project', 'speaker', 'department', 'topic']
+    actions = [make_published]
         
     def dept(self, obj):
         return "\n, ".join([p.name for p in obj.department.all()])
@@ -53,17 +60,13 @@ class eventadmin(admin.ModelAdmin):
 
 admin.site.register(Event, eventadmin)
 
-class pubcatadmin(admin.ModelAdmin):
+class pubcatadmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ('id','name')
     search_fields = ['name',]
 admin.site.register(Publication_category, pubcatadmin)
 
 
-@admin.action(description='Mark selected stories as published')
-def make_published(modeladmin, request, queryset):
-    queryset.update(publish=True)
-
-class publicationadmin(admin.ModelAdmin):
+class publicationadmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ('title','author', 'category','date_publish','dept', 'project', 'publish')
     list_filter = ('publish','department', 'category')
     search_fields = ['title','authors__name']
@@ -80,26 +83,42 @@ class publicationadmin(admin.ModelAdmin):
 
 admin.site.register(Publication, publicationadmin)
 
-class personadmin(admin.ModelAdmin):
+class personadmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ('name','position','organization','category','dept', 'is_active')
     list_filter = ('category','department',)
     search_fields = ['name','organization']
     date_hierarchy = 'date_created'
     readonly_fields = ('date_created', 'date_modified', )
     autocomplete_fields = ['department', 'expertise']
+    actions = [make_published]
         
     def dept(self, obj):
         return "\n, ".join([p.name for p in obj.department.all()])
 
 admin.site.register(Person, personadmin)
 
-class news_admin(admin.ModelAdmin):
+class news_admin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ('title','date_release','publish')
     search_fields = ['title']
     date_hierarchy = 'date_created'
     readonly_fields = ('date_created', 'date_modified')
+    actions = [make_published]
     
 admin.site.register(News, news_admin)
+
+
+class foundation_admin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ('Title','member','date_created','publish')
+    search_fields = ['title']
+    date_hierarchy = 'date_created'
+    readonly_fields = ('date_created', 'date_modified')
+    autocomplete_fields = ['member', ]
+    actions = [make_published]
+
+    def member(self, obj):
+        return "\n, ".join([p.name for p in obj.member.all()])
+    
+admin.site.register(Foundation, foundation_admin)
 
 
 
