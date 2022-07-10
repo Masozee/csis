cf84614b1baa2fae4e0015fda418b22293f9d6ae
masozee <nurojilukmansyah@gmail.com>
2022-07-10 17:02:28 +0700

    add admin

--- config/admin.py
+++ config/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 from .models import Slider
 
-admin.site.register(Slider)
+class SliderAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    search_fields = ['title', 'url', 'image', 'show', 'date_created']
+admin.site.register(Slider, SliderAdmin)
