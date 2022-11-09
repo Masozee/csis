b07d9151d4eebcca76f6eef407cd5e420e16848f
dev csis <dev@csis.or.id>
2022-11-09 14:13:37 +0700

    adding FAQ and partnership

--- config/admin.py
+++ config/admin.py
@@ -15,3 +15,11 @@ admin.site.register(CodebookOrder, CDAdmin)
 class CareerAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ('title', 'date_created', 'date_modified')
 admin.site.register( Career, CareerAdmin)
+
+class partnershipAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ('name', 'url', 'kategori', 'date_modified', 'publish')
+admin.site.register( Partnership, partnershipAdmin)
+
+class faqAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ('question', 'answer', 'date_modified', 'publish')
+admin.site.register( FAQ, faqAdmin)
--- config/models.py
+++ config/models.py
@@ -51,3 +51,45 @@ class Career(models.Model):
         value = self.title
         self.slug = slugify(value, allow_unicode=True)
         super().save(*args, **kwargs)
+
+class Partnership(models.Model):
+    N = '0'
+    I = '1'
+
+    KATEGORI_CHOICES = (
+        (N, 'Nasional'),
+        (I, 'Internasional'),
+    )
+
+    name = models.CharField(max_length=200)
+    url = models.URLField(blank=True, null=True)
+    logo = models.ImageField(upload_to='partnership')
+    about = models.TextField(blank=True, null=True)
+    kategori = models.CharField(max_length=1, choices=KATEGORI_CHOICES)
+    keterangan = models.TextField(blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    publish = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = ("Partnership")
+        verbose_name_plural = ("Partnership")
+
+class FAQ(models.Model):
+    question = models.TextField()
+    answer = models.TextField()
+    keterangan = models.TextField(blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    publish = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.question
+
+    class Meta:
+        verbose_name = ("F.A.Q")
+        verbose_name_plural = ("F.A.Q")
