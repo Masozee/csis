7f2d9b84f5ab7e2992e62112160a39794e937c00
dev csis <dev@csis.or.id>
2023-07-26 17:41:44 +0700

    add dashboard

--- apps/dashboard/url.py
+++ apps/dashboard/url.py
@@ -5,5 +5,7 @@ from apps.dashboard import views as dashviews
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="dashboard/index.html"), name='dashhome'),
+    path('person/details', TemplateView.as_view(template_name="dashboard/user-profile.html"), name='profile-detail'),
+    path('person/', TemplateView.as_view(template_name="dashboard/user-list.html"), name='profile'),
 
 ]
--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,3 +1,3 @@
 from django.shortcuts import render
-
+from apps.dashboard.models import *
 # Create your views here.
